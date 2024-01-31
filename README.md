0fcc8407859e07330df1cc9114e762ad8271dafa
Arthur <arthurvininunes@gmail.com>
2024-01-31 14:34:48 -0300

    Concluído desktop

---
+++ README.md
@@ -0,0 +1,40 @@
+# Challenge Alura - Decodificador
+
+Breve descrição do projeto.
+
+## Tecnologias Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Descrição do Projeto
+
+Uma breve descrição do projeto e seus objetivos.
+
+## Acesso ao Projeto
+
+Você pode acessar o projeto através do GitHub Pages [aqui]().
+
+## Instalação
+
+1. Clone o repositório: `git clone https://github.com/ApenasUmSonhador/Challenge-Alura---Decodificador`
+2. Abra o arquivo `index.html` em seu navegador.
+
+## Uso
+
+Descreva como utilizar o projeto e forneça exemplos de código, se necessário.
+
+## Contribuição
+
+Contribuições são bem-vindas! Se você quiser contribuir com este projeto, siga as etapas abaixo:
+
+1. Faça um fork do projeto
+2. Crie uma nova branch: `git checkout -b minha-nova-feature`
+3. Faça commit das suas alterações: `git commit -m 'Adicione uma nova feature'`
+4. Faça push para a branch: `git push origin minha-nova-feature`
+5. Abra um pull request
+
+## Licença
+
+Este projeto está licenciado sob a [Licença MIT](LICENSE).
--- app.js
+++ app.js
@@ -1,22 +1,46 @@
-function criptografar() {
-    let texto = document.querySelector("input").value;
-    texto = texto.replace(/e/g, "enter")
-                 .replace(/i/g, "imes")
-                 .replace(/a/g, "ai")
-                 .replace(/o/g, "ober")
-                 .replace(/u/g, "ufat");
-    document.getElementById("resultado").innerHTML = texto;
-}
-function descriptografar() {
-    let texto = document.querySelector("input").value;
-    texto = texto.replace(/enter/g, "e")
-                 .replace(/imes/g, "i")
-                 .replace(/ai/g, "a")
-                 .replace(/ober/g, "o")
-                 .replace(/ufat/g, "u");
-    document.getElementById("resultado").innerHTML = texto;
+let saida;
+
+function confereVazio(texto) {
+    if (texto === "") {
+        document.getElementById("saida-inicio").style.display = "flex";
+        document.getElementById("saida-fim").style.display = "none";
+    }
+    else {
+        document.getElementById("saida-inicio").style.display = "none";
+        document.getElementById("saida-fim").style.display = "flex";
+    }
 }
 
-function confere(funcao,texto){
-    
+function criptografar() {
+    let texto = document.querySelector("textarea").value;
+    confereVazio(texto);
+    texto = texto.replace(/e/g, "enter")
+                .replace(/i/g, "imes")
+                .replace(/a/g, "ai")
+                .replace(/o/g, "ober")
+                .replace(/u/g, "ufat");
+    document.getElementById("texto-saida").innerHTML = texto;
+    saida = texto;
+}
+
+function descriptografar() {
+    let texto = document.querySelector("textarea").value;
+    confereVazio(texto);
+    texto = texto.replace(/enter/g, "e")
+                .replace(/imes/g, "i")
+                .replace(/ai/g, "a")
+                .replace(/ober/g, "o")
+                .replace(/ufat/g, "u");
+    document.getElementById("texto-saida").innerHTML = texto;
+    saida = texto;
+}
+
+function copiarTexto() {
+    navigator.clipboard.writeText(saida)
+        .then(() => {
+            console.log('Texto copiado para o clipboard: ' + saida);
+        })
+        .catch((error) => {
+            console.error('Erro ao copiar texto para o clipboard: ' + error);
+        });
 }
